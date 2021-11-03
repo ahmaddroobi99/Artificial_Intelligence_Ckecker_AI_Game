e99b75e27d5e00eaad4039e6532251e2b13d8dfb
ahmaddroobi99 <ahmad.droobi1999@gmail.com>
2021-11-03 10:01:34 +0200

    FinalFormated_Version_Droobi

--- MiniMax_AlphaBata_Algo/AlphBetaAlgorithm.py
+++ MiniMax_AlphaBata_Algo/AlphBetaAlgorithm.py
@@ -6,21 +6,21 @@ Red = (255, 0, 0)
 White = (255, 255, 255)
 
 
-def miniMax(position ,depth ,max_player ,game ) :
-    if depth ==0 or  position.winner() !=None:
-        return position.evaluate(),position
+def miniMax(position, depth, max_player, game):
+    if depth == 0 or position.winner() != None:
+        return position.evaluate(), position
 
-    if max_player :
-        maxEval= float('-inf')
-        best_move =None
-        for move in get_all_moves (position,White,game):
-            evaluation  =miniMax(move ,depth -1 , False ,game)[0]
-            maxEval = max(maxEval,evaluation )
-            if maxEval ==evaluation  :
-                best_move =move
-        return  maxEval, best_move
-    else :
-        minEval = float('inf')
+    if max_player:
+        maxEval = float("-inf")
+        best_move = None
+        for move in get_all_moves(position, White, game):
+            evaluation = miniMax(move, depth - 1, False, game)[0]
+            maxEval = max(maxEval, evaluation)
+            if maxEval == evaluation:
+                best_move = move
+        return maxEval, best_move
+    else:
+        minEval = float("inf")
         best_move = None
         for move in get_all_moves(position, Red, game):
             evaluation = miniMax(move, depth - 1, True, game)[0]
@@ -30,35 +30,31 @@ def miniMax(position ,depth ,max_player ,game ) :
         return minEval, best_move
 
 
-
-def get_all_moves (board, color ,game ) :
+def get_all_moves(board, color, game):
     moves = []
-    for piece in board.get_all_pieces(color) :
+    for piece in board.get_all_pieces(color):
         valid_moves = board.get_valid_movies(piece)
-        for move,skip in valid_moves.items ():
+        for move, skip in valid_moves.items():
             # draw_moves(game,board,piece)
-            temp_board =deepcopy(board)
-            temp_piece = temp_board.get_piece (piece.row,piece.col)
-            new_board =simulate_move(temp_piece,move,temp_board,game,skip)
+            temp_board = deepcopy(board)
+            temp_piece = temp_board.get_piece(piece.row, piece.col)
+            new_board = simulate_move(temp_piece, move, temp_board, game, skip)
             moves.append(new_board)
 
     return moves
 
 
-def simulate_move (piece,move,board,game,skip) :
-    board.move(piece,move[0],move[1])
-    if skip :
+def simulate_move(piece, move, board, game, skip):
+    board.move(piece, move[0], move[1])
+    if skip:
         board.remove(skip)
     return board
-
-
 
 
 def draw_moves(game, board, piece):
     valid_moves = board.get_valid_movies(piece)
     board.draw(game.Win)
-    pygame.draw.circle(game.Win, (0,255,0), (piece.x, piece.y), 50, 5)
+    pygame.draw.circle(game.Win, (0, 255, 0), (piece.x, piece.y), 50, 5)
     game.draw_valid_moves(valid_moves.keys())
     pygame.display.update()
     # pygame.time.delay(100)
-
